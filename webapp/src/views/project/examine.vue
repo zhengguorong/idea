5b9d2b828083d8389aa4f190e5fd0dd3b9ef2be6
郑国榕 <zhengguorong@bluemoon.com.cn>
2017-08-09 08:27:41 +0800

    添加待审核

---
+++ webapp/src/views/project/examine.vue
@@ -0,0 +1,21 @@
+<template>
+  <List :list="examineList"/>
+</template>
+
+<script>
+import List from './list'
+import {mapGetters} from 'vuex'
+export default {
+  computed: {
+    ...mapGetters({
+      examineList: 'project/list'
+    })
+  },
+  components: {
+    List
+  },
+  mounted () {
+    this.$store.dispatch('project/getExamineList')
+  }
+}
+</script>
